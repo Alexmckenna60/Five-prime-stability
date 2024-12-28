0d5ed041fda07c0529aeac0f6d424a7c315007f6
Alexmckenna60 <168542015+Alexmckenna60@users.noreply.github.com>
2024-12-28 17:51:09 +0000

    Add files via upload

---
+++ Fig10/Fig10_Topt_core_GC3.r
@@ -0,0 +1,30 @@
+#Figure 10
+
+data1 <- read.csv("GC3_all_prop.csv",header=T)
+attach(data1)
+
+Temp<-unlist(data1[2])
+core.gc3<-unlist(data1[5])
+
+pdf("Fig10_Temp_Core_GC3.pdf")
+plot(Temp,core.gc3,pch=19,cex=0.7,xlab="Optimal growth temperature (°C)",ylab="Core GC3 content (%)",col="blue") #Make a plot for this aswell - to show correlation/interaction
+
+abline(lm(core.gc3~Temp),col="red")
+
+ct2 <-cor.test(Temp,core.gc3)
+ct2.p<-ct2$p.value
+ct2.rho<-ct2$estimate
+
+#Significant figures
+ctrho2 <- signif(ct2.rho, digits =3)
+ctp2 <- signif(ct2.p, digits =3)
+
+txt2<-paste("rho=",ctrho2,", p-value=",ctp2,sep="")
+mtext(txt2,side=3,adj=0.9,line=-2,cex=0.8)
+
+cor.test(Temp,core.gc3)
+
+dev.off()
+
+
+
---
+++ Fig13/Fig13_Diff_Topt_GC.r
@@ -0,0 +1,28 @@
+#Figure 13
+
+data1 <- read.csv("GC_all_prop.csv",header=T)
+attach(data1)
+
+Temp<-unlist(data1[2])
+core.gc<-unlist(data1[5])
+
+delta_core_f_10<-unlist(data1$Difference_between_core_and_first_10_codons_GC)
+
+pdf("Fig13_Delta_Temp_GC.pdf")
+plot(Temp,delta_core_f_10,pch=19,cex=0.7,xlab="Optimal growth temperature (°C)",ylab=expression(Delta ~ "GC"),col="blue")
+par(pty="s")
+abline(lm(delta_core_f_10~Temp),col="red")
+ct<-cor.test(delta_core_f_10,Temp,method="spearman")
+
+ct.p<-ct$p.value
+ct.rho<-ct$estimate
+
+#Significant figures
+ctrho <- signif(ct.rho, digits =3)
+ctp <- signif(ct.p, digits =3)
+
+txt<-paste("rho=",ctrho,", p-value=",ctp,sep="")
+mtext(txt,side=3,adj=0.9,line=-2,cex=0.8)
+dev.off()
+
+
